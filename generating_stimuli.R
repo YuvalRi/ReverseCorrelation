0b2ae3e36d20d98d9e474c919894601f9586757f
YuvalRi <yuvalritsker@gmail.com>
2022-09-09 17:41:26 +0300

    initial commit

---
+++ generating_stimuli.R
@@ -0,0 +1,4 @@
+# install packages 
+install.packages("viridisLite")
+install.packages("rcicr")
+
---
+++ rcicr_analysis.R
@@ -0,0 +1,25 @@
+# libraries
+library(viridisLite)
+library(rcicr)
+
+# Base image name used during stimulus generation
+baseimage <- 'base'
+
+# File containing the contrast parameters (this file was created during stimulus generation)
+rdata <- 'rcicrdemo.Rdata'
+
+# Load response data
+responsedata <- read.csv('rcicrdemo.csv')
+
+# Batch generate classification images by subject
+cis <- batchGenerateCI2IFC(responsedata, 'subject', 'stimulus', 'response', baseimage, rdata)
+
+# anti classification images (CIs) by subject
+anti_cis <- batchGenerateCI2IFC(responsedata, 'subject', 'stimulus', 'response', baseimage, rdata, antiCI = TRUE, label='anti')
+
+# Batch generate classification images by trait
+groupcis <- batchGenerateCI2IFC(responsedata, 'trait', 'stimulus', 'response', baseimage, rdata)
+
+# anti classification images (CIs) by trait
+
+anti_groupcis <- batchGenerateCI2IFC(responsedata, 'trait', 'stimulus', 'response', baseimage, rdata, antiCI = TRUE, label='anti')
